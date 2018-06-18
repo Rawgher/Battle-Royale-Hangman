fab62dec4ecf4e32d99df5eb398e2b015d9980f8
Roger <rdpender3@gmail.com>
2018-06-17 23:25:28 -0700

    starting

---
+++ assets/javascript/game.js
@@ -0,0 +1,2 @@
+var scifiWords = ["blaster", "spaceship", "asteroid", "cruiser", "laser", "starship", "space", "lightspeed"];
+var randomWord = scifiWords[Math.floor(Math.random() * scifiWords.length)];
