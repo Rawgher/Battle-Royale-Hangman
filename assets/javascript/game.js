d812fc650fa9a6d3e7c18d60e8c27e279e1b2e11
Roger <rdpender3@gmail.com>
2018-06-20 23:48:20 -0700

    getting there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,6 +32,12 @@ var word = wordList[Math.floor(Math.random() * wordList.length)];
 //     }
 // }
 
+//this should update what the current game shows on screen
+
+// function continuedGame() {
+    
+// }
+
 //chosen word is replaced with __
 function start() {
   for (i = 0; i < word.length; i++) {
@@ -68,19 +74,27 @@ function checkLetter() {
   }
 }
 
-// //function youWin() {
-//     if 
-// }
+function youWin() {
+ for (var i = 0; i < letters.length; i++) {   
+    if (letters[i] === "__") {
+        return false;
+        }
+    } return true;
+    wins++;
+}
 
-// function youLose() {
-//     if (guessesLeft === 0) {
-//         console.log("You lose");
-//     }
-// }
+function youLose() {
+    if (guessesLeft === 0) {
+        alert("You lose");
+        losses++;
+    }
+}
 
 
 start();
 checkLetter();
+youWin();
+youLose();
 
 
 
@@ -108,124 +122,102 @@ checkLetter();
 
 // reference DOM get elementsbyID for any placeholders, guessedletters, guessesleft, wins and losses
 // var $guessedLetters = document.getElementById("guessed-letters");
-// var $newWord = document.getElementById("chosen-word");
+// var $randomWord = document.getElementById("chosen-word");
 // var $incorrectLetters = document.getElementById("wrong-choices");
 // var $guessesLeft = document.getElementById("guesses-left");
 // var $wins = document.getElementById("wins");
 // var $losses = document.getElementById("losses");
 
 
-
-// create words to use and randomize them // 
-
-// var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-// var guess; //users guess
-// var letters = []; //correctly guessed letters
-// var incorrectLetters = [];
-// var counter = 0; //do i need this?
-// var guessesLeft = 13;
+// youtube here
 
 // var scifiWords = ["blaster", "spaceship", "asteroid", "cruiser", "laser", "starship", "space", "lightspeed", "star destroyer"];
 // var randomWord = scifiWords[Math.floor(Math.random() * scifiWords.length)];
-
-
-// function start() {
-//     for (i = 0; i <randomWord.length; i++) {
-//         letters[i] = "__";
-//     }
-
-//     document.getElementById("chosen-word").textContent = letters.join("");
-// }
-
-// function checkLetter() {
-//     document.onKeyUp = function(event) {
-//         guess = event.key.toLowerCase();
-//         var found = false;
-//         for (i = 0; i < randomWord.length; i++) {
-//             if (guess === randomWord[i]) {
-//                 letters[i] = guess;
-//                 document.getElementById("chosen-word").textContent = letters.join("");
-//                 found = true;
-//             }
-//         }
-//         if (found) return;
-
-//         if(incorrectLetters.indexOf(guess) < 0) {
-//             incorrectLetters.push(guess);
-//             guessesLeft--;
-//             document.getElementById("wrong-choices").textContent = incorrectLetters.join("");
-//         }
-//     }
-// }
-
-// start();
-// checkLetter();
-
-
-
-// make variables for wins losses and, picked placeholder picked word
-// want to automatically start game
-
 // var wins = 0;
 // var losses = 0;
-// var gameRunning = true;
 // var guessesLeft = 13;
-// var newWord = "";
-// var randomWordArray = [];
-// var guessedLetterArray = [];
-// var incorrectLetterArray = [];
 
+// var randomWordHolder = [];
+// var guessedLetters = [];
+// var incorrectLetters = [];
 
-//  {
-//     // resetting game
+// function newGame() {
 //     guessesLeft = 13;
-//     incorrectLetterArray = [];
-//     randomWordArray = [];
+//     guessedLetters = [];
+//     incorrectLetters = [];
+//     randomWordHolder = [];
 
+//     randomWord;
 
-//     // picking a word
-//     newWord = randomWord;
-
-//     // making placeholders of word
 //     for (var i = 0; i < newWord.length; i++) {
 //         if (newWord[i] === " ") {
-//             randomWordArray.push(" ");
+//             randomWordHolder.push(" ");
 //         } else {
-//             randomWordArray.push("_");
+//             randomWordHolder.push("_");
 //         }
 //     }
-
 //     $guessesLeft.textContent = guessesLeft;
-//     $newWord.textContent = randomWordArray.join("");
-//     $incorrectLetters.textContent = incorrectLetterArray;
+//     $newWord.textContent = randomWordHolder.join("");
+//     $incorrectLetters.textContent = incorrectLetterHolder;
 // }
+// 
+
+
+
+
+
+
 
 // function letterGuess(letter) {
-//     console.log(letter);
 
-//     if (guessedLetterArray.indexOf(letter) === -1) {
-//         guessedLetterArray.push(letter);
+//     if (guessedLetters.indexOf(letter) === -1) {
+//         guessedLetters.push(letter);
 
-//         for (var i = 0; i > newWord.length; i++) {
-//             if (newWord[i] === letter) {
-//                 randomWordAarry[i] = letter;
+//         for (var i = 0; i < randomWord.length; i++) {
+//             if (randomWord[i].toLowerCase() === letter.toLowerCase) {
+//                 randomWordHolder[i] = letter;
 //             }
 //         }
 //     }
 
-//     $newWord.textContent = randomWordArray.join("");
+//     $randomWord.textContent = randomWordHolder.join("");
 
 // }
 
 // function checkIncorrect(letter) {
-//     if (randomWordArray.indexOf(letter) === -1) {
+//     if (randomWordHolder.indexOf(letter) === -1 && randomWordHolder.indexOf(letter.toUpperCase)) === -1 {
 //         guessesLeft--;
-//         incorrectLetterArray.push(letter);
-//         $guessedLetters.textContent = incorrectLetterArray.join(" ");
+//         incorrectLetters.push(letter);
+//         $guessedLetters.textContent = incorrectLetters.join(" ");
 //         $guessedLetters.textContent = guessesLeft;
 //     }
+//     checkLoss();
+// )
     
-    
+// function checkLoss() {
+//     if (guessesLeft === 0) {
+//         losses++;
+//         $losses.textContent = losses;
+//         alert("you suck");
+//         gameReset;
+//     }
+// }
+
+// function checkWin() {
+//     if (randomWord.toLowerCase() === randomWordHolderjoin("").toLowerCase()) {
+//         wins++;
+//         alert("you win");
+//         $wins.textContent = wins;
+//     }
+//     checkWin();
+// }
+
+    // document.onKeyUp = function(event) {
+//     if (event.keyCode >= 65 && event.keyCode <= 90) {
+//         letterGuess(event.key);
+//     }
+
+// }
 
 // }
 
@@ -254,9 +246,14 @@ checkLetter();
 // //document.getElementById("wins").textContent = "wins: " + wins;
 // //document.getElementById("losses").textContent = "losses: " + losses;
 
-// document.onKeyUp = function(event) {
-//     if (event.keyCode >= 65 && event.keyCode <= 90) {
-//         letterGuess(event.key);
-//     }
 
-// }
+
+
+// create words to use and randomize them // 
+
+// var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+// var guess; //users guess
+// var letters = []; //correctly guessed letters
+// var incorrectLetters = [];
+// var counter = 0; //do i need this?
+// var guessesLeft = 13;
