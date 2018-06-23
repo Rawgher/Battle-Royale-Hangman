541cb93ee5eb923d9f42c45b7fa3ae57da9d83c0
Roger <rdpender3@gmail.com>
2018-06-22 21:11:08 -0700

    page keeps crashing

--- assets/javascript/word-object.js
+++ assets/javascript/word-object.js
@@ -1,73 +1,53 @@
-// lol don't look at these you cheater!
+
 myWords = [
     {
-        word: "",
-        image: "assets/images/",
-        description: ""
+        word: "FORTNITE",
+        image: "assets/images/fortnite.jpg",
+        description: "This is the most popular battle royale at the moment."
     },
     {
-        word: "",
-        image: "assets/images/",
-        description: ""
+        word: "PLAYER UNKNOWN BATTLEGROUNDS",
+        image: "assets/images/pubg.jpg",
+        description: "This is what started the craze, but the developers really dropped the ball."
+    },
+    // {
+    //     word: "h1z1",
+    //     image: "assets/images/h1z1.jpg",
+    //     description: "I thought this was about zombies or something?"
+    // },
+    {
+        word: "CUISINE ROYALE",
+        image: "assets/images/cuisine-royale.jpg",
+        description: "Using cooking utensils and guns? Who would've thought of this one?"
     },
     {
-        word: "",
-        image: "assets/images/",
-        description: ""
+        word: "TOTALLY ACCURATE BATTLEGROUNDS",
+        image: "assets/images/totally-accurate.jpg",
+        description: "All I have to say is that it's pretty accurate..."
     },
     {
-        word: "",
-        image: "assets/images/",
-        description: ""
+        word: "REALM ROYALE",
+        image: "assets/images/realm-royale.jpg",
+        description: "A mix of medieval warfare and some sorcery."
     },
     {
-        word: "",
-        image: "assets/images/",
-        description: ""
+        word: "RULES OF SURVIVAL",
+        image: "assets/images/rules-of-survival.jpg",
+        description: "First rule of survival... Survive."
     },
     {
-        word: "",
-        image: "assets/images/",
-        description: ""
+        word: "DARWIN PROJECT",
+        image: "assets/images/darwin-project.jpg",
+        description: "Show that you're the fittest by surviving."
     },
     {
-        word: "",
-        image: "assets/images/",
-        description: ""
+        word: "RADICAL HEIGHTS",
+        image: "assets/images/radical-heights.jpg",
+        description: "If you want something that's a little more hipster, play this one."
     },
     {
-        word: "",
-        image: "assets/images/",
-        description: ""
-    },
-    {
-        word: "",
-        image: "assets/images/",
-        description: ""
-    },
-    {
-        word: "",
-        image: "assets/images/",
-        description: ""
-    },
-    {
-        word: "",
-        image: "assets/images/",
-        description: ""
-    },
-    {
-        word: "",
-        image: "assets/images/",
-        description: ""
-    },
-    {
-        word: "",
-        image: "assets/images",
-        description: ""
-    },
-    {
-        word: "",
-        image: "assets/images/",
-        description: ""
+        word: "CALL OF DUTY BLACKOUT",
+        image: "assets/images/blackout.png",
+        description: "I'm a little sad that CoD jumped on the bandwagon."
     },
 ];
